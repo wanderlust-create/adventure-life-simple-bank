cbe6492df5f2b71c048a27bc3c44d858fa3dfd1c
Tamara Dowis <tamara.dowis@gmail.com>
2023-06-08 12:59:14 -0600

    create global TestDB var

--- simple_bank_db/main_test.go
+++ simple_bank_db/main_test.go
@@ -16,13 +16,15 @@ const (
 
 // define a testQueries object as a global variable
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
