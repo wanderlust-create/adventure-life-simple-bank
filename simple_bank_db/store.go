172066eedde0c5587040ff77b982cde89e5718dc
Tamara Dowis <tamara.dowis@gmail.com>
2023-06-08 13:00:13 -0600

    create Store for db transactions

---
+++ simple_bank_db/store.go
@@ -0,0 +1,95 @@
+package simple_bank_db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// provides all functions to run db queries individually as well as their combination within a transaction
+// provides all functions to execute Queries and transactions
+// Querry struct can only execute 1 operation at a time so this will expad it's functionality
+// is called a Composition: the preferred way to extend struct functionality instead of inheritance
+// embedding Queries inside of Store
+type Store struct {
+	*Queries
+	db *sql.DB // this is required to create a new DB transaction
+}
+
+// function to create a new Store Object
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+// executes a function within a db transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+// contains all the imput params of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// performes a money transfer from one account to the other.
+// It creates a tfr record, adds acct entries, and updates account balances within a single transaction
+// uses a Closure: used when we want to get the result from a callback function: result.Transfer: the callback function itself does not know the exact type result it shoud return
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TDO: update account balance
+		return nil
+	})
+	return result, err
+}
