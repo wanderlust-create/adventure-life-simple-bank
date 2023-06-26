864fb5c02f7af82a2245d9cf1cbf4dc81cea816b
Tamara Dowis <tamara.dowis@gmail.com>
2023-06-26 12:16:38 -0600

    api create account happy path test

--- api/account_test.go
+++ api/account_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	mockdb "github.com/wanderlust-create/adventure-life-simple-bank/db/mock"
@@ -17,8 +18,9 @@ import (
 	"github.com/wanderlust-create/adventure-life-simple-bank/util"
 )
 
-func TestGetAccountApi(t *testing.T) {
-	account := randomAccount()
+func TestGetAccountAPI(t *testing.T) {
+	userName := util.RandomOwnerName()
+	account := randomAccount(userName)
 
 	testCases := []struct {
 		name          string
@@ -104,11 +106,71 @@ func TestGetAccountApi(t *testing.T) {
 	}
 
 }
+func TestCreateAccountAPI(t *testing.T) {
+	userName := util.RandomOwnerName()
+	account := randomAccount(userName)
 
-func randomAccount() db.Account {
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore) // use this MockStore to build the stub that suits the purpose for each case
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"owner":    account.Owner,
+				"currency": account.Currency,
+				"balance":  account.Balance,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.CreateAccountParams{
+					Owner:    account.Owner,
+					Currency: account.Currency,
+					Balance:  0,
+				}
+				store.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusCreated, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/accounts"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+
+}
+
+func randomAccount(userName string) db.Account {
 	return db.Account{
 		ID:       util.RandomInt(1, 1000),
-		Owner:    util.RandomOwnerName(),
+		Owner:    userName,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
